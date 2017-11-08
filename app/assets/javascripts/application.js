f851f255c6247af4dd10edd5d72eee24cbc81b2b
Vaiva Margeleviciute <vaivav11@gmail.com>
2017-11-08 13:25:42 +0000

    done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//=require 'jquery3'
+//=require'bootstrap-sprockets'
 //= require rails-ujs
 //= require turbolinks
-//= require_tree .
+//= require_tree
+
+
+// application.js (require bootstrap after jquery)
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Homeslice',
+    address:      'Neal`s Yard, London',
+    category:     'italian',
+    phone_number: '555'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    phone_number: '333'
+  },
+
+    {
+    name:         'Yard Sale Pizza',
+    address:      'In Highbury, near Finsbury Park, London',
+    category:     'italian',
+    phone_number: '555'
+  },
+  {
+    name:         'Pizza Union',
+    address:      'Somewhere near King`s Cross, London',
+    category:     'italian',
+    phone_number: '333'
+  },
+
+    {
+    name:         'Pizza Piligrims',
+    address:      'Soho, London',
+    category:     'italian',
+    phone_number: '555'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
